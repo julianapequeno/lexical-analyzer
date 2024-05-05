647575cc15fb3b9a9c22d0341bed29e57e0fdd11
julianapequeno <julianafpsc@gmail.com>
2024-05-05 13:56:13 -0300

    initial files

---
+++ lexical-analyzer/src/main.rs
@@ -0,0 +1,23 @@
+structAnalisador { /* ... */ }
+
+implAnalisador {
+
+   fnnovo(entrada: String) -> Self {
+
+       todo!()
+
+   }
+
+   fn próximo(&mut self) -> (bool, usize, String) {
+
+       todo!()
+
+   }
+
+   fn devolver(&mut self, pos: usize, s: String) {
+
+       todo!()
+
+   }
+
+}
